b0c4a9eb162b8f938d3eff210de91578b26b6df5
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-09-06 22:04:52 +0530

    address of a array

---
+++ adrdessofarray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr)/4;
+    cout<<&arr[0]<<endl;
+    cout<<&arr[1]<<endl;
+    cout<<&arr[2]<<endl;
+}
