69bab41d56864f2959a8334b7c194b930c07c613
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-09-06 23:12:41 +0530

    passing array to function

--- adrdessofarray.cpp
+++ adrdessofarray.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9};
     int n = sizeof(arr)/4;
+    cout<<arr<<endl;
     cout<<&arr[0]<<endl;
     cout<<&arr[1]<<endl;
     cout<<&arr[2]<<endl;
