b4726e3a458aa3a3052fa5e7aa448b1feb10dfee
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-09-07 23:56:53 +0530

    vector basic

---
+++ Vectors.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int>arr(5);
+//     for(int i=0; i<5; i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr(5,18);
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+}
